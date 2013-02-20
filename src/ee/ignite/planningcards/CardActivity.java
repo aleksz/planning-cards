182d56848f2acb9928cbaadc6cd22b66cd114645
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2013-02-20 22:06:35 +0200

    basic grid view with hardcoded cards

--- src/ee/ignite/planningcards/CardActivity.java
+++ src/ee/ignite/planningcards/CardActivity.java
@@ -1,15 +1,44 @@
 package ee.ignite.planningcards;
 
-import android.os.Bundle;
+import static java.util.Collections.singletonMap;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
 
 public class CardActivity extends Activity {
+
+	private static String[] CARDS = new String[] { "1", "2", "3", "5", "8",
+			"13", "?", "C", "~" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.card);
+		setContentView(R.layout.cards);
+        configureCards();
+	}
+
+	private void configureCards() {
+		GridView cardGrid = (GridView) findViewById(R.id.card_grid);
+
+        List<Map<String, String>> gridData = new ArrayList<Map<String,String>>();
+
+        for (String cardValue : CARDS) {
+        	gridData.add(singletonMap("value", cardValue));
+        }
+
+		cardGrid.setAdapter(new SimpleAdapter(
+				this,
+				gridData,
+				R.layout.card,
+				new String[] { "value" },
+				new int[] { R.id.card_value }));
 	}
 
 	@Override
